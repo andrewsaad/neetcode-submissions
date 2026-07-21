be4be4e1eaba6bb5e4b2470f36952d69bde7912e
Andrew Saad <andrew.s.saad@gmail.com>
2026-07-20 22:13:05 -0700

    Add: python-custom-heap - submission-0

---
+++ Python Coding Interviews/python-custom-heap/submission-0.py
@@ -0,0 +1,21 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    heap = []
+    max_value =[]
+    for num in nums:
+        pair = (-(num),num)
+        heapq.heappush(heap,pair)
+    
+    while heap:
+        max_value.append(heapq.heappop(heap)[1])
+    return max_value
+
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
